061c58c4ef8f3234d8856fa6ecbd2da2384c99c5
hiteacheryouare <82683251+hiteacheryouare@users.noreply.github.com>
2022-05-04 16:47:45 -0400

    feat: :sparkles: initalize css stuffs
    node + tailwind + serve + fun stuff

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: ['index.html', 'index.js'],
+  theme: {
+    fontFamily: {
+      primary: ['Red Hat Display, sans-serif'],
+      specialty: ['Abril Fatface, cursive'],
+    },
+    extend: {
+      colors: {
+        'primary-dark': '#0D3B66',
+        'primary-light': '#52D1DC',
+        'specialty-red': '#F95738',
+        'specialty-orange': '#EE964B',
+        'specialty-yellow': '#F4D35E',
+      },
+    }
+  },
+}

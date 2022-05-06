3fd6bee0be2b485019188528333f9448a6b488ba
HedgehogDubz <HedgehogDubz@users.noreply.github.com>
2022-05-06 16:15:29 -0400

    pixel art can draw in one color now

--- pixelart.js
+++ pixelart.js
@@ -7,16 +7,33 @@ $(document).ready(function () {
     $("#canvas-map")[0].width = canvasSize;
     $("#canvas-map")[0].height = canvasSize;
     ctxmap = $("#canvas-map")[0].getContext("2d");
+    CreatePixelDataMap();
     setInterval(function () {
         Update();
-    }, 50);
+    }, 5);
 })
+
 function Update() {
     ctxmap.clearRect(0, 0, canvasSize, canvasSize);
     var pixelSize = canvasSize / sizeMap;
+
+    if (mouseDown) {
+        DrawOnMap();
+    }
+
+
+    DrawPixelDataMap();
+
     if (I_SeeLines()) {
         DrawLines();
     }
+
+
+
+
+
+
+
     if (I_MouseInBoundsMap()) {
         //console.log(mouseY);
         DrawRectMap(Math.floor(mouseX / pixelSize) * pixelSize, Math.floor(mouseY / (pixelSize)) * pixelSize, pixelSize, pixelSize, 0.2)
@@ -35,7 +52,48 @@ function DrawLines() {
     }
 }
 
-function DrawRectMap(x, y, w, h, a) {
+var pixelDataMap = [];
+function CreatePixelDataMap() {
+    for (var x = 0; x < sizeMap; x++) {
+        pixelDataMap.push([]);
+        for (var y = 0; y < sizeMap; y++) {
+            pixelDataMap[x].push([""]);
+        }
+    }
+    console.log(pixelDataMap);
+}
+function DrawPixelDataMap() {
+    var pixelSize = canvasSize / sizeMap;
+
+    var rowNum = 0;
+    pixelDataMap.forEach(function (row) {
+        var pixNum = 0;
+        row.forEach(function (pix) {
+
+            if (pix != "") {
+                DrawRectMap(rowNum * pixelSize, pixNum * pixelSize, pixelSize + 1, pixelSize + 1, 1, pix);
+            } else {
+                DrawRectMap(rowNum * pixelSize, pixNum * pixelSize, pixelSize / 2 + 1, pixelSize / 2 + 1, 1, "white");
+                DrawRectMap(rowNum * pixelSize + pixelSize / 2, pixNum * pixelSize, pixelSize / 2 + 1, pixelSize / 2 + 1, 1, "lightgray");
+                DrawRectMap(rowNum * pixelSize, pixNum * pixelSize + pixelSize / 2, pixelSize / 2 + 1, pixelSize / 2 + 1, 1, "lightgray");
+                DrawRectMap(rowNum * pixelSize + pixelSize / 2, pixNum * pixelSize + pixelSize / 2, pixelSize / 2 + 1, pixelSize / 2 + 1, 1, "white");
+
+
+            }
+            pixNum++;
+        })
+        rowNum++;
+    })
+}
+function DrawOnMap() {
+    var pixelSize = canvasSize / sizeMap;
+
+    pixelDataMap[Math.floor(mouseX / pixelSize)][Math.floor(mouseY / pixelSize)] = color;
+}
+
+function DrawRectMap(x, y, w, h, a, c) {
+    ctxmap.save();
+    ctxmap.fillStyle = c;
     if (a != null) {
         ctxmap.globalAlpha = a;
     } else {
@@ -43,7 +101,9 @@ function DrawRectMap(x, y, w, h, a) {
     }
     ctxmap.fillRect(x, y, w, h);
     ctxmap.globalAlpha = 1.0;
+    ctxmap.restore();
 }
+
 
 
 
@@ -63,6 +123,18 @@ function DrawRectMap(x, y, w, h, a) {
 //Input
 let mouseX = 0;
 let mouseY = 0;
+let mlbMode = "brush";
+let mrbMode = "erase";
+let color = "black";
+var mouseDown = 0;
+window.onload = () => {
+    document.body.onmousedown = function () {
+        ++mouseDown;
+    }
+    document.body.onmouseup = function () {
+        --mouseDown;
+    }
+}
 function GetMousePosMap(evt) {
     var rect = $("#canvas-map")[0].getBoundingClientRect();
     mouseX = evt.clientX - rect.left;
@@ -85,3 +157,4 @@ function I_SeeLines() {
     return $("#switch-seeLines").is(":checked");
     
 }
+
